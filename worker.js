efd38aae5f5663ec8151bcee474f3893dc63ccda
Dulandinuja <dulandinuja@ilcoud.com>
2026-03-04 09:44:51 +0530

    set frontend

---
+++ worker.js
@@ -0,0 +1,30 @@
+const BACKEND = 'http://187.124.98.120:8081';
+const ALLOWED_ORIGIN = 'https://sameerarice.cloud';
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': ALLOWED_ORIGIN,
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
+
+export default {
+  async fetch(request) {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, { headers: corsHeaders });
+    }
+
+    const url = new URL(request.url);
+    const backendUrl = BACKEND + url.pathname + url.search;
+
+    const response = await fetch(new Request(backendUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined,
+    }));
+
+    return new Response(response.body, {
+      status: response.status,
+      headers: { ...Object.fromEntries(response.headers), ...corsHeaders },
+    });
+  }
+};
